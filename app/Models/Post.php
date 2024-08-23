54ffc91f6580f1f8b5f36a5211c4e60fa3ecdb74
Leonard Kumaro <kumaro.2208541041@student.unud.ac.id>
2024-08-23 12:56:10 +0800

    membuat post category

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,9 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo; // Add this line
 
 class Post extends Model
 {
     use HasFactory;
     protected $fillable = ['title', 'author', 'slug', 'body'];
+
+    public function author(): BelongsTo // Update the return type
+    {
+        return $this->belongsTo(User::class, 'author_id');
+    }
+
+    public function category(): BelongsTo // Add this method
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
+
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -3,11 +3,16 @@
 
   @foreach($posts as $post)
     <article class="py-8 max-w-screen-md border-b border-gray-300">
-      <a href="/posts/{{ $post['slug'] }}" class="hover:underline">
+      <a href="/posts/{{ $post['slug'] }}" class="hover:underline text-base text-gray-500">
         <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">{{ $post['title'] }}</h2>
       </a>
-      <div class="text-base text-gray-500">
-        <a href="#">{{ $post['author'] }}</a> | {{ $post->created_at->diffForHumans() }}
+      <div >
+        By
+        <a href="/authors/{{ $post->author->username }}" class="hover:underline text-base text-gray-500">{{ $post->author->name }}</a>
+        in
+        <a href="/categories/{{ $post->category->slug }}" class="hover:underline text-base text-gray-500">{{ $post->category->name }}</a>
+        | 
+        {{ $post->created_at->diffForHumans() }}
       </div>
       <p class="my-4 font-light">
         {{ Str::limit($post['body'], 150) }}
